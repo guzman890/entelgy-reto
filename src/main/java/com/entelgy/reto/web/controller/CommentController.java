d82a0157b2865f8514cf834bc093950b1b4a1444
Pablo Guzman <guzmanfernando890@gmail.com>
2021-01-09 12:55:17 -0500

    a presentar

--- src/main/java/com/entelgy/reto/web/controller/CommentController.java
+++ src/main/java/com/entelgy/reto/web/controller/CommentController.java
@@ -4,7 +4,7 @@ import com.entelgy.reto.constants.Constants;
 import com.entelgy.reto.model.dto.DataDTO;
 import com.entelgy.reto.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
-    @GetMapping
+    @PostMapping
     public DataDTO getComments(){
         return commentService.getCommentList();
     }
